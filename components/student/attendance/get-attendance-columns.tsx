5ac57d9b41b07deafb78a581e6c6173d1f452ff9
Mike <mikemenos95@gmail.com>
2025-08-09 14:42:49 +0300

    fixes build

--- components/student/attendance/get-attendance-columns.tsx
+++ components/student/attendance/get-attendance-columns.tsx
@@ -6,14 +6,14 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
+  // DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import DeleteUserFromClerkButton from "@/components/delete-user-button";
-import { MembershipsResponse } from "@/types/membership";
-import RenewSubscriptionButton from "@/components/renew-subscription-button";
+// import DeleteUserFromClerkButton from "@/components/delete-user-button";
+// import { MembershipsResponse } from "@/types/membership";
+// import RenewSubscriptionButton from "@/components/renew-subscription-button";
 import { AttendanceT } from "@/types/attendance.type";
 
 export const getAttendanceColumns = (): ColumnDef<AttendanceT>[] => [
